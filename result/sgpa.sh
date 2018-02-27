a2393e8425eb2998e92aea67a0780e1b9bc154c7
farha kareem <farhakareem998@gmail.com>
2018-02-27 23:34:58 +0530

    csb cgpa

--- result/sgpa.sh
+++ result/sgpa.sh
@@ -18,8 +18,8 @@ sed -i 's/MA102/4/g' sem2t.txt
 sed -i 's/BE100/4/g' sem2t.txt
 sed -i 's/BE102/3/g' sem2t.txt
 sed -i 's/EC100/3/g' sem2t.txt
-sed -i 's/CS100/3/g' sem2t.txt
-sed -i 's/CS120/1/g' sem2t.txt
+sed -i 's/,CS100/ 3/g' sem2t.txt
+sed -i 's/,CS120/ 1/g' sem2t.txt
 sed -i 's/EC110/1/g' sem2t.txt
 sed -i 's/CY110/1/g' sem2t.txt
 sed -i 's/,/ /g' sem2t.txt
@@ -47,8 +47,10 @@ sed -i 's/BE103/3/g' sem1t.txt
 sed -i 's/EE100/3/g' sem1t.txt
 sed -i 's/PH110/1/g' sem1t.txt
 sed -i 's/EE110/1/g' sem1t.txt
-sed -i 's/CS110/1/g' sem1t.txt
+sed -i 's/,CS110/ 1/g' sem1t.txt
 sed -i 's/,/ /g' sem1t.txt
 awk '{ print ($1" "($2 * $3 + $4 * $5 + $6 * $7 + $8 * $9 + $10 * $11 + $12 * $13 + $14 * $15 + $16 * $17 + $18 * $19) /23) }' sem1t.txt > sem1sl.txt
 paste sem1sl.txt sem2sl.txt | awk '{print $1,$2,$4}' > both.txt
 awk '{ print ($1" "($2 + $3) /2) }' both.txt > cgpa.txt
+wget http://14.139.184.212/ask/c4b/c4b.txt
+join -1 6 -2 1 -o 1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.2 c4b.txt cgpa.txt>1.txt
