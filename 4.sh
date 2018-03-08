c3989ee7240eb79fcffd2c88e54fcca665cb6caa
farha kareem <farhakareem998@gmail.com>
2018-03-08 23:25:46 +0530

    question 4

---
+++ 4.sh
@@ -0,0 +1,8 @@
+echo "currently logged on user: $USER"
+echo "current shell: $SHELL"
+echo "home directory: $HOME"
+echo "operating system: $(uname)"
+echo "current path settings: $PATH"
+echo "current working directory: $PWD"
+echo "number of users logged in:" 
+who --count
